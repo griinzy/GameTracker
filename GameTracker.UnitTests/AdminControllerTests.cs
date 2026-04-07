5070a78cae08e688b95990d76009269788857783
griinzy <kalin_stoqnov@abv.bg>
2026-04-07 14:51:39 +0300

    Wrote Unit tests for GameController and AdminController.

---
+++ GameTracker.UnitTests/AdminControllerTests.cs
@@ -0,0 +1,84 @@
+﻿using GameTracker.Services;
+using GameTracker.ViewModels;
+using GameTracker.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTracker.UnitTests
+{
+    [TestFixture]
+    class AdminControllerTests
+    {
+        private Mock<IGameService> _mockService;
+        private AdminController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockService = new Mock<IGameService>();
+            _controller = new AdminController(_mockService.Object);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _controller.Dispose();
+        }
+
+        [Test]
+        public async Task Edit_ReturnsNotFound_WhenGameIsNull()
+        {
+            _mockService
+                .Setup(s => s.GetGameForEditByIdAsync(1))
+                .ReturnsAsync((GameEditViewModel)null);
+
+            var result = await _controller.Edit(1);
+
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public async Task Edit_RedirectsToDetails_WhenValid()
+        {
+            var model = new GameEditViewModel { Id = 1 };
+
+            var result = await _controller.Edit(model) as RedirectToActionResult;
+
+            Assert.That(result.ActionName, Is.EqualTo("Details"));
+            Assert.That(result.ControllerName, Is.EqualTo("Game"));
+        }
+
+        [Test]
+        public async Task Delete_ReturnsNotFound_WhenGameIsNull()
+        {
+            _mockService
+                .Setup(s => s.GetGameForDeletionByIdAsync(1))
+                .ReturnsAsync((GameDeleteViewModel)null);
+
+            var result = await _controller.Delete(1);
+
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public async Task CreateGenre_RedirectsToIndex_WhenModelValid()
+        {
+            var result = await _controller.CreateGenre(new CreateGenreViewModel()) as RedirectToActionResult;
+
+            Assert.That(result.ActionName, Is.EqualTo("Index"));
+        }
+
+        [Test]
+        public async Task CreateDeveloper_RedirectsToIndex_WhenModelValid()
+        {
+            var result = await _controller.CreateDeveloper(new CreateDeveloperViewModel()) as RedirectToActionResult;
+
+            Assert.That(result.ActionName, Is.EqualTo("Index"));
+        }
+    }
+}
---
+++ GameTracker.UnitTests/GameControllerTests.cs
@@ -0,0 +1,65 @@
+﻿using GameTracker.Services;
+using GameTracker.ViewModels;
+using GameTracker.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTracker.UnitTests
+{
+    [TestFixture]
+    public class GameControllerTests
+    {
+        private Mock<IGameService> _mockService;
+        private GameController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockService = new Mock<IGameService>();
+            _controller = new GameController(_mockService.Object);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _controller.Dispose();
+        }
+
+        [Test]
+        public async Task Details_ReturnsNotFound_WhenGameIsNull()
+        {
+            _mockService
+                .Setup(s => s.GetGameDetailsByIdAsync(1))
+                .ReturnsAsync((GameDetailsViewModel)null);
+
+            var result = await _controller.Details(1);
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public async Task Details_ReturnsModel_WhenGameExists()
+        {
+            var game = new GameDetailsViewModel { Id = 1 };
+            _mockService
+                .Setup(s => s.GetGameDetailsByIdAsync(1))
+                .ReturnsAsync(game);
+
+            var result = await _controller.Details(1) as ViewResult;
+
+            Assert.That(result.Model, Is.EqualTo(game));
+        }
+
+        [Test]
+        public async Task Create_RedirectsToIndex_WhenModelIsValid()
+        {
+            var result = await _controller.Create(new GameCreateViewModel()) as RedirectToActionResult;
+
+            Assert.That(result.ActionName, Is.EqualTo("Index"));
+        }
+    }
+}
